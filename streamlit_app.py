c3bfdce24984ebc3c6d0e9a9487301cc191ee20b
Hrishikesh Uchake <uchakeh@gmail.com>
2025-07-21 20:03:36 -0500

    Docker-Compose for automated deployments

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ from model import recommend_talks
 
 st.set_page_config(page_title="TED Talk Recommender", layout="wide")
 
-st.title("TED Talk Recommender")
-
+st.markdown("<h1><span style='color:red'>TED Talk</span> Recommender</h1>", unsafe_allow_html=True)
 user_input = st.text_area("Enter a TED Talk topic idea:", height=100)
 
 if st.button("Get Recommendations") and user_input.strip():
